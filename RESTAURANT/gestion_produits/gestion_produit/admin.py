296fcb11a68386e497ff7a78aa1d736bda23aaa6
meditech24 <sow36aguibou@gmail.com>
2024-04-02 17:48:33 +0000

    gestion de la reservation dans ce commit nous avons gérer le systeme de reservation nous avons crées la views avec toute les logique de reservation qui serons misent à jours si nécessaire.

--- RESTAURANT/gestion_produits/gestion_produit/admin.py
+++ RESTAURANT/gestion_produits/gestion_produit/admin.py
@@ -1,9 +1,43 @@
+from django.core.mail import EmailMessage
+from django.conf import settings
 from django.contrib import admin
 from .models import Produits,Categorie,Order,Card,Commentaires,Reservation
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('nom','prix','date','categorie')
     search_fields = ('nom','categorie')
     ordering = ('date',)
+    
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('client', 'date','heure','reservation','statu')
+    actions = ['confirmer_reservation','annuler_reservation']
+
+    def confirmer_reservation(self, request, queryset):
+        for reservation in queryset:
+            reservation.statu = 'confirmée'
+            reservation.save()
+            subject = 'Confirmation reservation'
+            message = "votre reservation a été confirmé"
+            from_email = settings.EMAIL_HOST_USER
+            to_email = reservation.client.email
+            email = EmailMessage(subject,message,from_email,[to_email])
+            email.send()
+        self.message_user(request, "Les réservations sélectionnées ont été confirmées avec succès.")
+    confirmer_reservation.short_description = "Confirmer les réservations sélectionnées"
+    
+    
+    def annuler_reservation(self, request, queryset):
+        for reservation in queryset:
+            reservation.statu = 'annulée'
+            reservation.save()
+            subject = 'Annulation de la réservation'
+            message = "Votre réservation a été annulée"
+            from_email = settings.EMAIL_HOST_USER
+            to_email = reservation.client.email
+            email = EmailMessage(subject, message, from_email, [to_email])
+            email.send()
+        self.message_user(request, "Les réservations sélectionnées ont été annulées avec succès.")
+    annuler_reservation.short_description = "Annuler les réservations sélectionnées"
 
 # Register your models here.
 admin.site.register(Produits, ArticleAdmin)
@@ -11,7 +45,7 @@ admin.site.register(Categorie)
 admin.site.register(Order)
 admin.site.register(Card)
 admin.site.register(Commentaires)
-admin.site.register(Reservation)
+admin.site.register(Reservation, ReservationAdmin)
 
 
 admin.site.site_title=("Bienvenu dans mon site E-commerce") # pour chager le titre au niveau du login admin
